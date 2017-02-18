aea227d87be8c9936d3a1719618e488a6f159e83
Beverly Abadines <bev.abadines@gmail.com>
2017-02-18 15:48:20 -0800

    More Volunteers and Editable Basis Size
    All the Basis Training can be in the same directory. but the basis size
    is changeable.

--- knn_accuracy.m
+++ knn_accuracy.m
@@ -3,11 +3,11 @@ basis_dir = 'C:\Users\Beverly\Documents\GitHub\Hand_Gesture_Recognition\Training
 distinct = 5;
 
 % Basis of 5 different gestures from a large sample
-[basis,subjects,ROW,COL] = oneDirBasis(basis_dir,distinct);    
+[basis,subjects,ROW,COL] = oneDirBasis(basis_dir,distinct,14);    
 group = ['fist   ';'middle ';'pinky  ';'pointer';'thumb  '];    
 
 %call basis to receive subject, ROW, and COL from sample_dir
-[~,sampleSubjects,sampleROW,sampleCOL] = oneDirBasis(sample_dir,distinct);
+[~,sampleSubjects,sampleROW,sampleCOL] = oneDirBasis(sample_dir,distinct,16);
 % Create test sample of from excluded data 
 sample = allSamples(sample_dir, distinct,sampleSubjects,sampleROW,sampleCOL);
 
--- oneDirBasis.m
+++ oneDirBasis.m
@@ -1,4 +1,4 @@
-function [ basis,subjects,ROW,COL ] = oneDirBasis(directory,distinct)
+function [ basis,subjects,ROW,COL ] = oneDirBasis(directory,distinct,subject_basis_size)
 % Forms a training basis from multiple subjects' .csv files inside a single
 % directory for knnclassify. 
 % Requires a single directory which contains all the data from all subjects
@@ -11,18 +11,22 @@ function [ basis,subjects,ROW,COL ] = oneDirBasis(directory,distinct)
    
     i=1;
     subjects = num_files / distinct;
+    num_file_end = (num_files - (subjects - subject_basis_size)*distinct);
     basis = [];
     temp = [];
     % check if there is only one subject
     if (distinct ~= num_files)
         for n = 1 : distinct
-            for i = n :distinct :  num_files
-               
+            for i = n :distinct :  num_file_end
                 %removes name(row) and time(col)and only takes an even fraction of
                 %data from each subject's distinct gesture
-                b1_files = (csvread(fullfile(directory, basis1(i).name),1,1,[1,1,int8(ROW/subjects),COL]));
+                b1_files = (csvread(fullfile(directory, basis1(i).name),1,1,[1,1,int8(ROW/subject_basis_size),COL]));
                 %concatenates col values (8 meaningful values)
                 temp = vertcat(temp,b1_files);
+              %   Verify that it captures the right files and amount of data per subject
+              %   if (i == num_file_end)
+              %        b2_files = (csvread(fullfile(directory, basis1(i).name),1,1,[1,1,(ROW/subject_basis_size),COL]))
+              %  end
             end
             %sums col values
             basis(n,:) = sum(temp);
