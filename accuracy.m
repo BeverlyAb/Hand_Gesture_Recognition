b901dc0cf4ec4cd44da26ca9b3742389d9fb03de
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2017-02-18 21:24:48 -0800

    Update accuracy.m

--- accuracy.m
+++ accuracy.m
@@ -1,6 +1,6 @@
 function [ fraction,incorrect] = accuracy( class, sample_test_dir )
 % Outputs the accuracy of the gesture recognition, based on the file
-% names from the training basis folder and knn class output. This also returns a an array
+% names from the training basis folder and knn class output. This also returns an array
 % of the misidentified gestures. Note this also calls gestureName.m
     correct = 0;
     wrong = 0;
